323a53b1718a63aa5f09329b92c7631adf00e14b
Patrot <prabhupathrot@gmail.com>
2020-02-09 19:22:37 +0530

    First commit

---
+++ src/test/java/com/example/Springsocialfacebook/SpringSocialFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.Springsocialfacebook;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringSocialFacebookApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
