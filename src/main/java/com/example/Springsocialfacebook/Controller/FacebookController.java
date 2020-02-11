0fcbeb5db21967aa122626785bac1f6e39c8431f
Patrot <prabhupathrot@gmail.com>
2020-02-11 09:54:49 +0530

    removing accessing personal fields

--- src/main/java/com/example/Springsocialfacebook/Controller/FacebookController.java
+++ src/main/java/com/example/Springsocialfacebook/Controller/FacebookController.java
@@ -1,6 +1,10 @@
 package com.example.Springsocialfacebook.Controller;
 
+import com.example.Springsocialfacebook.Repository.UserDetailsRepo;
+import com.example.Springsocialfacebook.UserProfile.UserPojoDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.UserProfile;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
@@ -8,6 +12,7 @@ import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +23,8 @@ public class FacebookController {
 
     private ConnectionRepository connectionRepository;
 
+    @Autowired
+    private UserDetailsRepo userDetailsRepo;
 
     public FacebookController(Facebook facebook, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
@@ -31,10 +38,21 @@ public class FacebookController {
             return "redirect:/connect/facebook";
         }
         PagedList<Post> posts = facebook.feedOperations().getPosts();
-        User profile = facebook.userOperations().getUserProfile();
+//        User profile = facebook.userOperations().getUserProfile();
         model.addAttribute("profileName", posts.get(0).getFrom().getName());
-        model.addAttribute("dob",profile.getEmail());
+//        model.addAttribute("dob",profile.getEmail());
         model.addAttribute("posts", posts);
         return "profile";
     }
+
+//    public String userDetails(@PathVariable String id)
+//    {
+//        UserPojoDetails userPojoDetails;
+//        User profile = facebook.userOperations().getUserProfile();
+//        if(profile.getId() == id)
+//        {
+//            userPojoDetails = userDetailsRepo.save(profile);
+//        }
+//    }
+
 }
